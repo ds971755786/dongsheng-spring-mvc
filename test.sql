55694d44f95c1405ac967bf54178c0c3e93eb217
aarondongsheng <ds971755786@gmail.com>
2017-02-23 16:46:01 +0800

    add schema

--- test.sql
+++ test.sql
@@ -1,1 +1,1 @@
-insert into test (id, first_name, last_name, cellphone) values(2, 'Chunhua', 'Hu', '18534561111');
+insert into jira.test (id, first_name, last_name, cellphone) values(2, 'Chunhua', 'Hu', '18534561111');
